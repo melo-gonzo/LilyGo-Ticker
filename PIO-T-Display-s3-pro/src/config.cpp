bd032fce0cee38f9802e725abf4b6f1eb8efb2fa
Carmelo <carmelojsg@gmail.com>
2024-10-12 15:31:40 -0700

    refactor: renaming config file

---
+++ PIO-T-Display-s3-pro/src/config.cpp
@@ -0,0 +1,33 @@
+#include "config.h"
+
+// // Test intraday with fake data
+// bool USE_TEST_DATA = true;
+// bool USE_INTRADAY_DATA = true;
+// int INTRADAY_UPDATE_INTERVAL = 1; // 5
+// int CANDLE_COLLECTION_DURATION = 6; // 60
+// const char* STOCK_SYMBOL = "SPY";
+
+// Intraday with real data
+bool USE_TEST_DATA = false;
+bool USE_INTRADAY_DATA = true;
+int INTRADAY_UPDATE_INTERVAL = 5; // 5
+int CANDLE_COLLECTION_DURATION = 60; // 60
+const char* STOCK_SYMBOL = "SPY";
+
+// // // Daily data
+// bool USE_TEST_DATA = false;
+// bool USE_INTRADAY_DATA = false;
+// int INTRADAY_UPDATE_INTERVAL = 5; // 5
+// int CANDLE_COLLECTION_DURATION = 60; // 60
+// const char* STOCK_SYMBOL = "SPY";
+
+
+void loadConfig() {
+    // In the future, you could load these values from EEPROM or a file system
+    // For now, we'll just use the default values
+    // Example of how you might load from EEPROM:
+    // USE_TEST_DATA = EEPROM.read(0);
+    // USE_INTRADAY_DATA = EEPROM.read(1);
+    // EEPROM.get(2, INTRADAY_UPDATE_INTERVAL);
+    // ...
+}
